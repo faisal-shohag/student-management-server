d6b4081db1aa9aa0b7089a7685825acf3a57a3f1
Faisal Shohag <50203997+faisal-shohag@users.noreply.github.com>
2024-07-01 15:18:18 +0600

    added resources

--- API/get.js
+++ API/get.js
@@ -84,4 +84,18 @@ router.get('/courses/recordings/:id', async(req, res) => {
     }
 })
 
+router.get('/courses/resources/:id', async(req, res) => {
+    const id = req.params.id;
+    try {
+        const resource = await prisma.resources.findUnique({
+            where: {
+                id: Number(id)
+            }
+        })
+        res.status(200).json(resource)
+    } catch (error) {
+        res.status(500).json({ error: error.message })
+    }
+})
+
 export default router;
