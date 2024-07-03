6d24719817d453b1a0b921d0abd46a2b80cf3764
faisal-shohag <faisalshohagprog@gmail.com>
2024-07-03 16:32:23 +0600

    updated

--- index.js
+++ index.js
@@ -10,10 +10,7 @@ import put from './API/put.js';
 const app = express()
 const PORT = process.env.PORT || 5000
 
-app.use(cors({
-    origin: ['http://localhost:5173', 'http://localhost:5174', 'http://localhost:5175'],
-    credentials: true,
-  }))
+app.use(cors())
 
   app.use(express.json())
   app.use(express.urlencoded({ extended: true }))
